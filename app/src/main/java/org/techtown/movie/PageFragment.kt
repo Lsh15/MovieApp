080381980a2f3b5cbf4f11cdf6186740972c5798
SeunghHun <starzary@naver.com>
2022-05-28 16:29:22 +0900

    MovieApp : Chapter02 03 까지

--- app/src/main/java/org/techtown/movie/PageFragment.kt
+++ app/src/main/java/org/techtown/movie/PageFragment.kt
@@ -8,6 +8,25 @@ import android.view.ViewGroup
 
 class PageFragment : Fragment() {
 
+    var imageId = 0
+    var title:String? = null
+    var details1:String? = null
+    var details2:String? = null
+
+    companion object{
+        fun newInstance(imageId:Int, title:String?, details1:String?, details2: String?):PageFragment{
+            val fragment = PageFragment()
+
+            val bundle = Bundle()
+            bundle.putInt("imageId",imageId)
+            bundle.putString("title",title)
+            bundle.putString("details1",details1)
+            bundle.putString("details2",details2)
+            fragment.arguments = bundle
+
+            return fragment
+        }
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_page, container, false)
     }
