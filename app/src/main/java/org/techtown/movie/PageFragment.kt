aa7b8eda252c2e47d5a30a51ca1c20d4bdbb6a1c
SeunghHun <starzary@naver.com>
2022-05-29 16:05:52 +0900

    MovieApp : Chapter02 04 까지

--- app/src/main/java/org/techtown/movie/PageFragment.kt
+++ app/src/main/java/org/techtown/movie/PageFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_page.view.*
 
 class PageFragment : Fragment() {
 
@@ -27,8 +28,27 @@ class PageFragment : Fragment() {
             return fragment
         }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.apply {
+            imageId = getInt("imageId")
+            title = getString("title")
+            details1 = getString("details1")
+            details2 = getString("details2")
+
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_page, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_page, container, false)
+
+        rootView.posterImageView.setImageResource(imageId)
+        rootView.titleTextView.text = title
+        rootView.details1TextView.text = details1
+        rootView.details2TextView.text = details2
+
+        return rootView
     }
  
 }
